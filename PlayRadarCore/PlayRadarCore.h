da41ebbe73517e9aa2d89b0acb5873f11ce243ea
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-09 06:43:11 +0700

    Add PlayRadarCore framework

---
+++ PlayRadarCore/PlayRadarCore.h
@@ -0,0 +1,18 @@
+//
+//  PlayRadarCore.h
+//  PlayRadarCore
+//
+//  Created by Muhammad Muizzsuddin on 09/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayRadarCore.
+FOUNDATION_EXPORT double PlayRadarCoreVersionNumber;
+
+//! Project version string for PlayRadarCore.
+FOUNDATION_EXPORT const unsigned char PlayRadarCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayRadarCore/PublicHeader.h>
+
+
