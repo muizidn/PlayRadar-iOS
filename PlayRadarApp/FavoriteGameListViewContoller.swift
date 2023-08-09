5bfefb5a0c624929aeeec1ec58332906a5f1d929
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-09 09:44:59 +0700

    Add FavoriteGameListViewController

---
+++ PlayRadarApp/FavoriteGameListViewContoller.swift
@@ -0,0 +1,103 @@
+//
+//  GameListViewController.swift
+//  PlayRadarApp
+//
+//  Created by Muhammad Muizzsuddin on 09/08/23.
+//
+
+import UIKit
+import PlayRadar
+
+class FavoriteGameListViewController: UIViewController {
+    // MARK: - Properties
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(GameCell.self, forCellReuseIdentifier: GameCell.reuseIdentifier)
+        return tableView
+    }()
+    
+    let games: [GameViewModel] = [
+        GameViewModel(
+            id: "",
+            coverImage: URL(string: "https://media.rawg.io/media/resize/420/-/screenshots/d0e/d0e70feaab57195e8286f3501e95fc5e.jpg"),
+            title: "foo",
+            releaseDate: Date(),
+            rating: 4.5),
+        GameViewModel(
+            id: "",
+            coverImage: URL(string: "https://media.rawg.io/media/resize/420/-/screenshots/d0e/d0e70feaab57195e8286f3501e95fc5e.jpg"),
+            title: "foo",
+            releaseDate: Date(),
+            rating: 4.5),
+        GameViewModel(
+            id: "",
+            coverImage: URL(string: "https://media.rawg.io/media/resize/420/-/screenshots/d0e/d0e70feaab57195e8286f3501e95fc5e.jpg"),
+            title: "foo",
+            releaseDate: Date(),
+            rating: 4.5)
+    ]
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - UI Setup
+    
+    func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource & UITableViewDelegate
+
+extension FavoriteGameListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: GameCell.reuseIdentifier,
+            for: indexPath
+        ) as! GameCell
+        
+        let game = games[indexPath.row]
+        cell.configure(with: game)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return GameCell.cellHeight
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct FavoriteGameListViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ControllerPreviewContainer {
+            let vc = FavoriteGameListViewController()
+            vc.viewDidLoad()
+            return vc
+        }
+    }
+}
+#endif
